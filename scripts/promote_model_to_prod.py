9e533460af225b5d6cead706d5545a0ec7360deb
Amrith <amrithputtur@gmail.com>
2025-04-24 09:38:01 +0530

    ci test

--- scripts/promote_model_to_prod.py
+++ scripts/promote_model_to_prod.py
@@ -5,21 +5,11 @@ from pathlib import Path
 import mlflow
 
 
-
-
-
 import dagshub
 dagshub.init(repo_owner='AMR-ITH', repo_name='swiggy-delivery-time-estimator', mlflow=True)
 
 # set the tracking server
 mlflow.set_tracking_uri("https://dagshub.com/AMR-ITH/swiggy-delivery-time-estimator.mlflow")
-
-# pathlib is a module in Python that provides an object-oriented interface 
-# for working with file system paths.
-current = Path.cwd()
-parent = current.parent
-print(f"Current directory: {current}")
-print(f"Parent directory: {parent}")
 
 
 def load_model_information(file_path):
@@ -28,7 +18,7 @@ def load_model_information(file_path):
     return run_info
 
 # Define the model name and current alias
-model_name = load_model_information(parent / "run_information.json")["model_name"]
+model_name = load_model_information( "run_information.json")["model_name"]
 current_alias = "staging_latest"
 new_alias = "production_latest"
 # Get the model version associated with the current alias
@@ -37,3 +27,12 @@ model_version = model_version_info.version
 # Set the new alias for the model version
 mlflow.set_registered_model_alias(model_name, new_alias, model_version)
 print(f"Transitioned model version (model_version) from alias '{current_alias}' to '{new_alias}'.")
+
+# Update the tag from 'staging' to 'production'
+mlflow.set_model_version_tag(
+    name=model_name,
+    version=model_version,
+    key="deployment_stage",
+    value="production"
+)
+print(f"Transitioned model version (model_version) from alias '{current_alias}' to '{new_alias}' and updated deployment_stage to 'production'.")
