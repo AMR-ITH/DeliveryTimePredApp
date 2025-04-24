a7c12be1f4a30b7a24d419233a14fd0b875f1fd0
Amrith <amrithputtur@gmail.com>
2025-04-24 10:14:47 +0530

    ci test

--- scripts/promote_model_to_prod.py
+++ scripts/promote_model_to_prod.py
@@ -2,7 +2,7 @@ import mlflow
 import dagshub
 import json
 from pathlib import Path
-import mlflow
+from mlflow.tracking import MlflowClient
 
 
 import dagshub
@@ -17,19 +17,19 @@ def load_model_information(file_path):
         run_info = json.load(f)
     return run_info
 
+# Initialize the MLflow client
+client = MlflowClient()
 # Define the model name and current alias
-model_name = load_model_information( "run_information.json")["model_name"]
+model_name = load_model_information("run_information.json")["model_name"]
 current_alias = "staging_latest"
 new_alias = "production_latest"
 # Get the model version associated with the current alias
-model_version_info = mlflow.get_model_version_by_alias(model_name, current_alias)
+model_version_info = client.get_model_version_by_alias(model_name, current_alias)
 model_version = model_version_info.version
 # Set the new alias for the model version
-mlflow.set_registered_model_alias(model_name, new_alias, model_version)
-print(f"Transitioned model version (model_version) from alias '{current_alias}' to '{new_alias}'.")
-
+client.set_registered_model_alias(model_name, new_alias, model_version)
 # Update the tag from 'staging' to 'production'
-mlflow.set_model_version_tag(
+client.set_model_version_tag(
     name=model_name,
     version=model_version,
     key="deployment_stage",
